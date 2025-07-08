534792ce9bc62e93bf25be3081ab11886f9fba56
ChristopherJohnDestajo <c.destajo.546094@umindanao.edu.ph>
2025-07-09 00:12:24 +0800

    Created a LoginService with UnitTesting

---
+++ Consultation.BackEndCRUD/Repository/UserRepository.cs
@@ -0,0 +1,24 @@
+﻿using Consultation.BackEndCRUD.Repository.IRepository;
+using Consultation.Domain;
+using Consultation.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Consultation.BackEndCRUD.Repository
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly AppDbContext _context;
+
+        public UserRepository(AppDbContext context) => _context = context;
+
+        public Users? GetUserByEmail(string email)
+        {
+            return _context.Users.FirstOrDefault(u => u.Email == email);
+        }
+    }
+}
